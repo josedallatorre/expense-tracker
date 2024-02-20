d8e65204b519e01fda9d6725c1f0797eb04f945e
josedallatorre <jose.dallatorre@hotmail.com>
2024-02-20 16:31:25 +0100

    feat: added basic pandas df

--- tgbot/csv_reader.py
+++ tgbot/csv_reader.py
@@ -1,10 +1,7 @@
 import csv
-months = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre']
-tot: float = 0
+import pandas
+months = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre', 'Ottobre']
 for month in months:
     with open('Bilancino - '+month+'.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            tot = tot + float(row[' Ammontare'].replace(",",".").replace("€","").replace(" ",""))
-            print(row['Data'], row[' Ammontare'], row['Descrizione'])
-print("Tot: "+str(tot))
+        df = pandas.read_csv(csvfile)
+        print(df)
