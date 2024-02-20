6389c9a7625104d44a3c7846516b2f4ec7748f09
josedallatorre <jose.dallatorre@hotmail.com>
2024-02-20 18:38:38 +0100

    feat: using only tg for interacting

--- tgbot/csv_reader.py
+++ tgbot/csv_reader.py
@@ -4,4 +4,7 @@ months = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agos
 for month in months:
     with open('Bilancino - '+month+'.csv', newline='') as csvfile:
         df = pandas.read_csv(csvfile)
+        df = df.rename(columns={' Ammontare': 'Ammontare'})
+        df['Ammontare'] = df['Ammontare'].replace({'\€': ''}, regex=True)
+        df.loc["Total", " Ammontare"] = df.Ammontare.sum()
         print(df)
