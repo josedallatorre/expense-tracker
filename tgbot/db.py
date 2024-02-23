97556e3e75ad68c4bb20acbcc9bc8947975aec3e
josedallatorre <jose.dallatorre@hotmail.com>
2024-02-23 14:56:14 +0100

    feat: added insert dynamically, with autoincr transaction_id

--- tgbot/db.py
+++ tgbot/db.py
@@ -2,6 +2,8 @@ import psycopg2
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from psycopg2 import sql
+from datetime import date
 dotenv_path= Path('../.env')
 load_dotenv(dotenv_path=dotenv_path)
 HOST = os.getenv('host')
@@ -16,8 +18,19 @@ def connect():
                             password = PASSWORD,
                             port = 5432)
     cur = conn.cursor()
-    cur.execute("INSERT INTO \"CLIENT\"(username) VALUES ('jose');")
-    print("Data Inserted")
+    #cur.execute("INSERT INTO \"CLIENT\"(username) VALUES ('jose38');")
+    #print("Data Inserted")
+    # Make the changes to the database persistent
+    #conn.commit()
+    # Close cursor and communication with the database
+    #transaction_id = '10'
+    d = date.today()
+    value = 100
+    description = 'test'
+    user_id = 1
+    t  =  (d, value, description, user_id)
+    cur.execute("insert into \"TRANSACTION\" values(DEFAULT, %s,%s,%s,%s)", t)
+    print("Data dynamiccaly Inserted")
     # Make the changes to the database persistent
     conn.commit()
     # Close cursor and communication with the database
