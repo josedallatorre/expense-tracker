0d9ea8a63f000bb748b95e53f0003d57982a78e8
josedallatorre <jose.dallatorre@hotmail.com>
2024-02-23 18:41:36 +0100

    feat: import csv data into db

--- tgbot/csv_reader.py
+++ tgbot/csv_reader.py
@@ -1,28 +1,34 @@
 import pandas
-months = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto']
-for month in months:
+def open_csv(month):
     with open('Bilancino - '+month+'.csv', newline='') as csvfile:
         df = pandas.read_csv(csvfile)
-        df.drop(df.columns[[0]], axis=1, inplace=True)
-        df = df.rename(columns={' Ammontare': 'Ammontare'})
-        df['Ammontare'] = df['Ammontare'].replace({'\€': ''}, regex=True)
-        df['Ammontare'] = df['Ammontare'].apply(lambda x: x.replace('  ', ''))
-        df['Ammontare'] = df['Ammontare'].apply(lambda x: x.replace(' ', ''))
-        df['Ammontare'] = df['Ammontare'].apply(lambda x: x.replace(',', '.'))
-        df['Ammontare'] = df['Ammontare'].apply(lambda x: x.replace('.', ''))
-        #df[['AmmontareInt', 'AmmontareCent']] = df['Ammontare'].str.split('.', n=1, expand=True)
-        df['Ammontare'] = df['Ammontare'].astype(int)
-        #df['AmmontareInt'] = df['AmmontareInt'].astype(int)
-        #df['AmmontareCent'] = df['AmmontareCent'].astype(int)
-        df['Descrizione'] = df['Descrizione'].astype(str).str.cat(df['Unnamed: 4'].fillna('').astype(str), sep=' _ ').str.cat(df['Unnamed: 5'].fillna('').astype(str), sep=' _ ')
-        df.drop(df.columns[[4]], axis=1, inplace=True)
-        df.drop(df.columns[[3]], axis=1, inplace=True)
+    return df
 
-        print(df.dtypes)
-        """
-        TotalInt = df.AmmontareInt.sum()
-        TotalCent = df.AmmontareCent.sum()
-        """
-        Total = df.Ammontare.sum()
-        #print(df, "TotaleInt: " + str(TotalInt), "TotalCent: " +str(TotalCent), "Total: " +str(Total))
-        print(df,"\n"+ str(Total))
+def clean_df(df):
+    df.drop(df.columns[[0]], axis=1, inplace=True)
+    df = df.rename(columns={' Ammontare': 'Ammontare'})
+    df['Ammontare'] = df['Ammontare'].replace({'\€': ''}, regex=True)
+    df['Ammontare'] = df['Ammontare'].apply(lambda x: x.replace('  ', ''))
+    df['Ammontare'] = df['Ammontare'].apply(lambda x: x.replace(' ', ''))
+    df['Ammontare'] = df['Ammontare'].apply(lambda x: x.replace(',', '.'))
+    df['Ammontare'] = df['Ammontare'].apply(lambda x: x.replace('.', ''))
+    #df[['AmmontareInt', 'AmmontareCent']] = df['Ammontare'].str.split('.', n=1, expand=True)
+    df['Ammontare'] = df['Ammontare'].astype(int)
+    #df['AmmontareInt'] = df['AmmontareInt'].astype(int)
+    #df['AmmontareCent'] = df['AmmontareCent'].astype(int)
+    df['Descrizione'] = df['Descrizione'].astype(str).str.cat(df['Unnamed: 4'].fillna('').astype(str), sep=' _ ').str.cat(df['Unnamed: 5'].fillna('').astype(str), sep=' _ ')
+    df.drop(df.columns[[4]], axis=1, inplace=True)
+    df.drop(df.columns[[3]], axis=1, inplace=True)
+    print(df.dtypes)
+    """
+    TotalInt = df.AmmontareInt.sum()
+    TotalCent = df.AmmontareCent.sum()
+    """
+    #print(df, "TotaleInt: " + str(TotalInt), "TotalCent: " +str(TotalCent), "Total: " +str(Total))
+    return df
+def row(df, i):
+    Total = df.Ammontare.sum()
+    print(df,"\n"+ str(Total))
+    row = df.loc[i,:].values.flatten().tolist()
+    print(row)
+    return (row)
--- tgbot/main.py
+++ tgbot/main.py
@@ -1,5 +1,32 @@
-import bot as bot
+#import bot as bot
+from datetime import datetime
 import db
+import csv_reader
+import numpy as np
+from psycopg2.extensions import register_adapter, AsIs
+register_adapter(np.int64, AsIs)
 print("hello world")
-bot.echo_all
-db.connect
+
+months = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto']
+for month in months:
+    df = csv_reader.open_csv(month=month)
+    df_cleaned = csv_reader.clean_df(df)
+    n_rows = df_cleaned.__len__()
+    for row in range(n_rows):
+        t = csv_reader.row(df_cleaned,row)
+        data= t[0]
+        date_object = datetime.strptime(data, '%d/%m/%Y').date()
+        a = t[1].item()
+        descr = t[2]
+        user_id = 1
+        print(type(date_object))
+        print(date_object)  # printed in default format
+        print(a, type(a))
+        t1 = (date_object.isoformat(), a, descr, user_id)
+        cur, conn = db.connect()
+        db.insert(cur, conn, t1)
+        db.close(cur, conn)
+#bot.echo_all
+#db.connect
+
+
