2d39ac99dac523968025612b58324f0265a23f0b
josedallatorre <jose.dallatorre@hotmail.com>
2024-02-26 16:26:04 +0100

    cleanup of comments

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,15 +1,17 @@
 CREATE SCHEMA expense;
 
-CREATE TABLE "CLIENT" (
+CREATE TABLE CLIENT (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE "TRANSACTION" (
+CREATE TABLE TRANSACTION (
     transaction_id SERIAL PRIMARY KEY,
     date DATE,
     value NUMERIC NOT NULL,
     description VARCHAR(50),
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES "CLIENT" (user_id)
+    FOREIGN KEY (user_id) REFERENCES CLIENT (user_id)
 );
+
+INSERT INTO CLIENT(username) VALUES ('test');
