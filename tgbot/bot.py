002282eadd68b88567b42ae68a4823175c905109
josedallatorre <jose.dallatorre@hotmail.com>
2024-03-11 18:14:42 +0100

    feat: added document handler

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -27,7 +27,6 @@ def send_welcome(message):
 # Handles all text messages that match the regular expression for yyyy-mm-dd
 @bot.message_handler(regexp="(\d{4})-(\d{2})-(\d{2})")
 def handle_message(message):
-	bot.reply_to(message, "ho trovato una data")
 	t = message.text.split()
 	user_id = message.from_user.id
 	data= t[0]
@@ -35,14 +34,31 @@ def handle_message(message):
 	a = t[1]
 	descr = t[2]
 	t1 = (date_object.isoformat(), a, descr, user_id)
+	bot.reply_to(message, "Inserting with date: " + str(data)+ " amount " + str(a)+ " with description " + str(descr))
 	cur, conn = db.connect()
 	db.insert_transaction(cur, conn, t1)
 	db.close(cur, conn)
+	bot.reply_to(message, "Inserted correctly")
+
 
 # Handles all text messages 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
+
+# Handles all document, accept only csv file
+@bot.message_handler(content_types=['document'])
+def handle_document(message):
+	if not message.document.file_name.endswith('.csv'):
+		bot.reply_to(message, "Must be a csv file")
+	else:
+		bot.reply_to(message, "Saving the csv")
+		file_name = message.document.file_name
+		file_info = bot.get_file(message.document.file_id)
+		downloaded_file = bot.download_file(file_info.file_path)
+		with open('./' + file_name, 'wb') as new_file:
+			new_file.write(downloaded_file)
+
 
 
 bot.infinity_polling()
